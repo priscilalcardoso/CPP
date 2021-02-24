039854bc6c8669d22af7a77aab787e2eb03ae02f
Priscila Luiza Cardoso <66534381+priscilalcardoso@users.noreply.github.com>
2021-02-24 17:32:03 -0300

    Teste dos comandos C++

---
+++ teste.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int inteiro = 5;
+    cout << inteiro << endl;
+
+    float real; 
+    real = 5.2;
+    cout << real << endl;
+
+    bool booleano;
+    booleano = false;
+    cout << booleano << endl;
+    
+    char letra;
+    letra = 'b';
+    cout << letra << endl;
+
+    string palavra;
+    palavra = "bola";
+    cout << palavra << endl;
+
+    int idade;
+    cout << "Qual sua idade? " << endl;
+    cin >> idade; 
+    cout << idade << endl;
+
+
+    return 0;
+}
