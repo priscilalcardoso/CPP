05256511e3d755fe844706cf3a5673c958c56ae3
Priscila Luiza Cardoso <66534381+priscilalcardoso@users.noreply.github.com>
2021-02-27 17:37:19 -0300

    Olá mundo em C

---
+++ OlaMundoC.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h> //biblioteca necessária para entrada de dados
+#include<stdlib.h> //necessário para system("pause" do windows)
+int main (void)
+{//inicio
+
+	printf("ola mundo \n");
+	system("pause"); //pausa na tela so funciona no windowns
+	return 0;//valor de retorno da função main.
+
+	
+}//fim do programa
